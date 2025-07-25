70fec76ee84f1033f5f1139239250034ebef7399
dshparko <951008d@mail.ru>
2025-07-25 18:14:16 +0300

    feat: add scripts for creating & initializing table

---
+++ user-service/src/main/resources/sql/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (username, firstname, lastname, email, password, role)
+VALUES ('ivanov', 'Ivan', 'Ivanov', 'ivan@gmail.com', '', 'ADMIN'),
+       ('petrov', 'Petr', 'Petrov', 'petr@gmail.com', '', 'USER'),
+       ('svetik', 'Sveta', 'Svetikova', 'sveta@gmail.com', '', 'USER'),
+       ('vladik', 'Vlad', 'Vladikov', 'vlad@gmail.com', '', 'USER'),
+       ('ksmith', 'Kate', 'Smith', 'kate@gmail.com', '', 'ADMIN');
---
+++ user-service/src/main/resources/sql/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id        BIGSERIAL PRIMARY KEY,
+    username  VARCHAR(64) NOT NULL UNIQUE,
+    firstname VARCHAR(64),
+    lastname  VARCHAR(64),
+    email     VARCHAR(255),
+    password  varchar(128) default '{noop}123',
+    role      VARCHAR(32)
+);
