de787e3307812b1c8da49ab4ff9fe9a73a3ad55c
dshparko <951008d@mail.ru>
2025-07-25 18:01:53 +0300

    init: initialize projects

---
+++ notification-service/src/test/java/by/dshparko/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.dshparko.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
