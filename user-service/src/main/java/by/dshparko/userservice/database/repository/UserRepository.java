eda8e24f13a863a4693ccf652e24b2bcb7b5635e
dshparko <951008d@mail.ru>
2025-07-26 21:58:19 +0300

    feat: create service for authentication

--- user-service/src/main/java/by/dshparko/userservice/database/repository/UserRepository.java
+++ user-service/src/main/java/by/dshparko/userservice/database/repository/UserRepository.java
@@ -2,9 +2,13 @@ package by.dshparko.userservice.database.repository;
 
 import by.dshparko.userservice.database.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByUsername(String username) throws UsernameNotFoundException;
 }
---
+++ user-service/src/main/java/by/dshparko/userservice/service/CustomUserService.java
@@ -0,0 +1,28 @@
+package by.dshparko.userservice.service;
+
+import by.dshparko.userservice.database.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserService implements UserDetailsService {
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username)
+                .map(user -> new org.springframework.security.core.userdetails.User(
+                        user.getUsername(),
+                        user.getPassword(),
+                        Collections.singleton(user.getRole())
+                ))
+                .orElseThrow(() -> new UsernameNotFoundException("Failed to retrieve user " + username));
+
+    }
+}
